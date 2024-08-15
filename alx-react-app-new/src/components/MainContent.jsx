bbcb9eaedbe81447890de05ffc05d612ec56ab51
Abel-G <abeljeb@gmail.com>
2024-08-15 22:22:53 +0300

    week 10, task 0

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <main>
+        <p style={{backgroundColor: 'crimson', color: 'white', padding: '20px'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default MainContent
