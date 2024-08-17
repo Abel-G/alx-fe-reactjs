eebcdd79066c8e402a2b7b0ec880b60fd96ff720
Abel-G <abeljeb@gmail.com>
2024-08-17 09:21:05 +0300

    adding decrement and increment on button

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -5,8 +5,8 @@ function Counter() {
   return (
     <div>
         <p>{count}</p>
-        <button onClick={() =>{setCount(count + 1)}}>Plus</button>
-        <button onClick={() =>{setCount(count - 1)}}>Minus</button>
+        <button onClick={() =>{setCount(count + 1)}}>Increment</button>
+        <button onClick={() =>{setCount(count - 1)}}>Decrement</button>
         <button onClick={() =>{setCount(0)}}>Reset</button>
     </div>
   )
