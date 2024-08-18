dc6c37243f365e57984b0d0ba217bb3b7e6dbd56
Abel-G <abeljeb@gmail.com>
2024-08-18 22:34:00 +0300

    adding insline style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const customStyle ={
+        backgroundColor: 'grey',
+         display:'inline-block', 
+         justifyContent: 'center',
+         listStyle: 'none',
+         margin: '10px',
+         padding: '10px',
+    }
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li style ={customStyle}><Link to="/">Home</Link></li>
+        <li style ={customStyle}><Link to="/about">About Us</Link></li>
+        <li style ={customStyle}><Link to="/services">Services</Link></li>
+        <li style ={customStyle}><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
