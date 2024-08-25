b05c22288dfcbadb91ed3f12c561ae0ee5ecee7a
Abel-G <abeljeb@gmail.com>
2024-08-25 17:41:08 +0300

    print

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,19 @@
-import { useRecipeStore } from './recipeStore'
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
+import RecipeCard from './RecipeCard'; 
+const RecipeList = () => {
+  const { filteredRecipes } = useRecipeStore();
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
-
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
+  return (
+    <div>
+      <SearchBar /> 
+      <ul>
+        {filteredRecipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
-      </div>
-    );
-  };
-  export default RecipeList;
+      </ul>
+    </div>
+  );
+};
+
+export default RecipeList;
