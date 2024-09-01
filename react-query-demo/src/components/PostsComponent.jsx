51f6e2684a8119ab5ee596397d3dbc2782f4d8c2
Abel-G <abeljeb@gmail.com>
2024-09-01 14:52:21 +0300

    add

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
-import { isError, useQuery } from 'react-query';
+import { useQuery } from 'react-query';
 
-export const fetchPosts = async () => {
+const fetchPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   return response.json();
 };
 
-export const PostsComponent = () => {
-    const { isLoading, error, data, refetch } = useQuery('posts', fetchPosts);
-  
-    if (isLoading) return <p>Loading posts...</p>;
-  
-    if (isError) return <p>Error: {error.message}</p>;
-  
-    return (
-      <div>
-        <h2>Posts</h2>
-        <ul>
-          {data.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-        <button onClick={refetch}>Refetch Posts</button>
-      </div>
-    );
-  };
+const PostsComponent = () => {
+  const { isLoading, isError, error, data, refetch } = useQuery('posts', fetchPosts, {
+    cacheTime: 1000 * 60 * 3,
+    staleTime: 1000 * 60 * 1,
+    refetchOnWindowFocus: true,
+    keepPreviousData: true,
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <button onClick={refetch}>Refetch Posts</button>
+    </div>
+  );
+};
+
+export default PostsComponent;
