f94a27bdde5a3ff624c116dbb80b8042a24f61a8
Abel-G <abeljeb@gmail.com>
2024-09-14 22:53:24 +0300

    print

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import data from '../data.json';
-
+import { Link } from 'react-router-dom';
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
 
@@ -16,9 +16,11 @@ const HomePage = () => {
           <div key={recipe.id} className='shadow-lg rounded overflow-hidden hover:shadow-xl'>
             <img src={recipe.image} alt={recipe.title} className='w-full object-cover h-48'/>
             <div className='py-4'>
-              <h2 className='font-bold'>{recipe.title}</h2>
+              <h2 className='font-bold'><Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link></h2>
               <p className='text-gray-400'>{recipe.summary}</p>
-              <a href={`/recipes/${recipe.id}`} className='hover:text-blue-950'>View Recipe</a>
+              <Link to={`/recipe/${recipe.id}`} className="text-blue-500 hover:text-blue-700">
+                View Recipe
+              </Link>
             </div>
           </div>
         ))}
