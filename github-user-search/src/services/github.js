86ac952ac4919e3b5aeffe281d76b58a21acf7d2
Abel-G <abeljeb@gmail.com>
2024-09-20 20:35:20 +0300

    setting up env

---
+++ github-user-search/src/services/github.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_GITHUB_API_URL || 'https://api.github.com';
+
+export const getUser = async (username) => {
+  try {
+    const response = await axios.get(`${apiUrl}/users/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user', error);
+    throw error;
+  }
+};
